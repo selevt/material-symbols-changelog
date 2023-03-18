393658ea28105fed428e1e69bb4296fc322d3ac5
selevt <selevt@gmail.com>
2023-03-18 13:26:15 +0100

    Prepare page for initial content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,20 +12,22 @@
     }
 </script>
 
-<h1>Material Symbols Changelog</h1>
+<!-- Hide layout until there is content -->
+{#if data.changelog?.length}
+    <h1>Material Symbols Changelog</h1>
 
-<p>
-    This is an inofficial changelog for <a href="https://fonts.google.com/icons">Material Symbols</a>
-    and not affiliated with Google or the Material Symbols project.
-</p>
+    <p>
+        This is an inofficial changelog for <a href="https://fonts.google.com/icons">Material Symbols</a>
+        and not affiliated with Google or the Material Symbols project.
+    </p>
 
-{#each data.changelog as version}
-    <section>
-        <!-- <h2 id='version-{version.date}'>{new Date(version.date).toLocaleDateString()} | v{version.version}</h2> -->
-        <h2 id='version-{version.date}'>{formatTimestamp(version.date)} <sup>v{version.version}</sup></h2>
+    {#each data.changelog as version}
+        <section>
+            <h2 id='version-{version.date}'>{formatTimestamp(version.date)} <sup>v{version.version}</sup></h2>
 
-        <IconBoxes title='Added' icons={version.added}></IconBoxes>
-        <IconBoxes title='Removed' icons={version.removed}></IconBoxes>
-        <IconBoxes title='Changed' icons={version.changed}></IconBoxes>
-    </section>
-{/each}
+            <IconBoxes title='Added' icons={version.added}></IconBoxes>
+            <IconBoxes title='Removed' icons={version.removed}></IconBoxes>
+            <IconBoxes title='Changed' icons={version.changed}></IconBoxes>
+        </section>
+    {/each}
+{/if}
