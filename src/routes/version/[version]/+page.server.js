4a82854961e30b1939c783a221552535e7d86b72
selevt <selevt@gmail.com>
2023-05-01 10:03:33 +0200

    RSS Feed
    Provide /rss route with a feed of the 20 latest updates.

--- src/routes/version/[version]/+page.server.js
+++ src/routes/version/[version]/+page.server.js
@@ -1,6 +1,6 @@
 import { error } from '@sveltejs/kit';
 import changelog from '$lib/../../data/changelog.json'
-import { filterSmallChanges, formatTimestamp } from '$lib/utils';
+import { buildDescription, filterSmallChanges } from '$lib/utils';
 
 export const prerender = true;
 export const csr = false;
@@ -25,24 +25,4 @@ export async function load({params}) {
             description: buildDescription(filteredVersion),
         },
     };
-}
-
-/**
- * @param {import('$lib/types').Version} version
- * @returns {string}
- */
-function buildDescription(version) {
-    /** @type {string[]} */
-    const parts = []
-    if (version.added.length > 0) {
-        parts.push(`${version.added.length} added`)
-    }
-    if (version.changed.length > 0) {
-        parts.push(`${version.changed.length} changed`)
-    }
-    if (version.removed.length > 0) {
-        parts.push(`${version.removed.length} removed`)
-    }
-
-    return `Material Symbols changes from ${formatTimestamp(version.date)}: ${parts.join(', ')}`;
 }
